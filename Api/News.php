44f995772ca20b1c017478254d41456d186af3d8
Oliver Kossin <oliver.kossin@brandung.de>
2020-06-28 11:43:24 +0200

    Remove old Copy paste errors

--- Api/News.php
+++ Api/News.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of Sulu.
- *
- * (c) Sulu GmbH
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace App\Bundle\NewsBundle\Api;
 
 use JMS\Serializer\Annotation\ExclusionPolicy;
