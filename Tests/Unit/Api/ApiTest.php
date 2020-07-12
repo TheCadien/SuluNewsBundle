4ae782943557069080f67f3caf375b45301a2eeb
Oliver Kossin <thecadiens@gmail.com>
2020-07-12 15:54:19 +0200

    Add Empty Dto Check

---
+++ Tests/Unit/Api/ApiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TheCadien/SuluNewsBundle.
+ *
+ * (c) Oliver Kossin
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace TheCadien\Bundle\SuluNewsBundle\Tests\Unit\Api;
+
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+    public function testEmptyApiDto()
+    {
+
+    }
+}
