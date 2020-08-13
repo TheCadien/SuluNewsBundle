1234872b37eeb33b03817fee857c1011085f92b2
Oliver Kossin <thecadiens@gmail.com>
2020-08-13 17:46:51 +0200

    Add Admin Form Unit Tests

---
+++ Tests/Functional/Controller/AdminControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TheCadien/SuluNewsBundle.
+ *
+ * (c) Oliver Kossin
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Functional\Controller;
+
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
+
+class AdminControllerTest extends SuluTestCase
+{
+    public function testTagMetadataAction()
+    {
+        $client = $this->createAuthenticatedClient();
+
+        $client->request('GET', '/admin/metadata/form/news_details');
+
+        $this->assertHttpStatusCode(200, $client->getResponse());
+        $response = json_decode($client->getResponse()->getContent());
+
+        $form = $response->form;
+
+        $this->assertObjectHasAttribute('title', $form);
+        $this->assertObjectHasAttribute('publishedAt', $form);
+        $this->assertObjectHasAttribute('header', $form);
+        $this->assertObjectHasAttribute('teaser', $form);
+        $this->assertObjectHasAttribute('content', $form);
+        $this->assertObjectHasAttribute('teaser', $form);
+
+        $schema = $response->schema;
+
+        $this->assertSame(['title', 'publishedAt'], $schema->required);
+    }
+}
