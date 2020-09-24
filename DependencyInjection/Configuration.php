02ffe0b22eedddb33e1a3c7491295507c471f284
Oliver Kossin <thecadiens@gmail.com>
2020-09-24 11:54:24 +0200

    Add Config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -28,9 +28,11 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('news_bundle');
-        $treeBuilder->getRootNode()
-            ->children()
+        $treeBuilder = new TreeBuilder('sulu_news');
+        $root = $treeBuilder->getRootNode();
+
+
+        $root->children()
                 ->arrayNode('objects')
                     ->addDefaultsIfNotSet()
                     ->children()
