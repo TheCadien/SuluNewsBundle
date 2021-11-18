2ed3f7de73adaea1e7c0b63fefd74da15df802e5
Oliver Kossin <thecadiens@gmail.com>
2021-11-18 16:30:08 +0100

    Refactor preview

--- Preview/NewsObjectProvider.php
+++ Preview/NewsObjectProvider.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace TheCadien\Bundle\SuluNewsBundle\Preview;
 
 use Sulu\Bundle\PreviewBundle\Preview\Object\PreviewObjectProviderInterface;
+use TheCadien\Bundle\SuluNewsBundle\Entity\News;
 use TheCadien\Bundle\SuluNewsBundle\Repository\NewsRepository;
 
 class NewsObjectProvider implements PreviewObjectProviderInterface
@@ -28,12 +29,12 @@ class NewsObjectProvider implements PreviewObjectProviderInterface
         $this->newsRepository = $newsRepository;
     }
 
-    public function getObject($id, $locale)
+    public function getObject($id, $locale): ?News
     {
-        return $this->newsRepository->findById((int) $id);
+        return $this->newsRepository->findById((int)$id);
     }
 
-    public function getId($object)
+    public function getId($object): string
     {
         return $object->getId();
     }
@@ -43,7 +44,7 @@ class NewsObjectProvider implements PreviewObjectProviderInterface
         // TODO: Implement setValues() method.
     }
 
-    public function setContext($object, $locale, array $context)
+    public function setContext($object, $locale, array $context) :News
     {
         if (\array_key_exists('template', $context)) {
             $object->setStructureType($context['template']);
@@ -52,12 +53,12 @@ class NewsObjectProvider implements PreviewObjectProviderInterface
         return $object;
     }
 
-    public function serialize($object)
+    public function serialize($object): string
     {
         return serialize($object);
     }
 
-    public function deserialize($serializedObject, $objectClass)
+    public function deserialize($serializedObject, $objectClass): News
     {
         return unserialize($serializedObject);
     }
