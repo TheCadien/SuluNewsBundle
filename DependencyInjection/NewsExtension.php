f0b90291275f08411b5e22b60a7e51634cb6465d
Oliver Kossin <thecadiens@gmail.com>
2022-11-09 23:32:04 +0100

    Fix and add News selection again

--- DependencyInjection/NewsExtension.php
+++ DependencyInjection/NewsExtension.php
@@ -41,7 +41,7 @@ class NewsExtension extends Extension implements PrependExtensionInterface
                 'sulu_search',
                 [
                     'indexes' => [
-                        'contact' => [
+                        'news' => [
                             'name' => 'News',
                             'icon' => 'su-newspaper',
                             'view' => [
@@ -92,6 +92,34 @@ class NewsExtension extends Extension implements PrependExtensionInterface
                             'routes' => [
                                 'list' => 'app.get_news',
                                 'detail' => 'app.get_news',
+                            ],
+                        ],
+                    ],
+                    'field_type_options' => [
+                        'selection' => [
+                            'news_selection' => [
+                                'default_type' => 'list_overlay',
+                                'resource_key' => News::RESOURCE_KEY,
+                                'view' => [
+                                    'name' => 'app.news_edit_form',
+                                    'result_to_view' => [
+                                        'id' => 'id',
+                                    ],
+                                ],
+                                'types' => [
+                                    'auto_complete' => [
+                                        'display_property' => 'title',
+                                        'search_properties' => ['title'],
+                                    ],
+                                    'list_overlay' => [
+                                        'adapter' => 'table',
+                                        'list_key' => 'news',
+                                        'display_properties' => ['title'],
+                                        'label' => 'sulu_news.news_select',
+                                        'icon' => 'su-newspaper',
+                                        'overlay_title' => 'sulu_news.single_news_selection_overlay_title',
+                                    ],
+                                ],
                             ],
                         ],
                     ],
