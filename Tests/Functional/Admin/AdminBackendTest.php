7abdc09f540a3d6f8b365b68f7069776d821b7cf
Oliver Kossin <thecadiens@gmail.com>
2023-07-13 16:52:33 +0200

    Run cs-fixer

--- Tests/Functional/Admin/AdminBackendTest.php
+++ Tests/Functional/Admin/AdminBackendTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 namespace Functional\Admin;
 
 use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
